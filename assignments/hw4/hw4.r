48717ef84b785f814fb69fe6e2209b57bd3c4673
Pranay Singal <psingal@berkeley.edu>
2015-02-26 23:29:24 -0800

    Partially complete

--- assignments/hw4/hw4.r
+++ assignments/hw4/hw4.r
@@ -14,10 +14,29 @@
 #   element of <data.list>
 
 listLengths <- function(data.list) {
-
-    # your code here
-
+  length_datalist <- length(data.list)
+  i <- 1
+  element.lengths <- c()
+  while (i <= length_datalist){
+    length_vector <- length(data.list[[i]])
+    element.lengths <- c(element.lengths, length_vector)
+    i <- i + 1
+    }
+  return(element.lengths)
 }
+#first compute length of list, n
+#create a loop that iterates from 1 to n
+#inside loop, compute length of the sub-list, set as a variable
+#insert variable into a list variable
+#return list
+
+x <- list(c(1:4), c(3, 4), c(2:7))
+class(x)
+length(x)
+x[1]
+y <- list()
+length(y)
+y[1]
 
 #### Function 2
 #### Implement the function "powers"
@@ -31,10 +50,21 @@ listLengths <- function(data.list) {
 #              the column names should be : "x", "x^2", "x^3" etc.
 
 powers <- function(x, k){
-
+  x.powers <- data.frame(x)
+  for (i in 2:k){
+    column <- x**i
+    x.powers <- cbind(x.powers, column)
+  } 
+  column_names <- paste("x^", as.character(2:k), sep = "")
+  colnames(x.powers) <- c("x", column_names)
+  return(x.powers)
 }
 
- 
+x <- c(1:4)
+x
+y <- x**2
+y[3]
+
 #### Function #3
 #### Implement the function "recipeConversion"
 
@@ -64,8 +94,33 @@ powers <- function(x, k){
 
 # Put your code here
 recipeConversion <- function(recipe){
-
+  if (colnames(recipe)[1] != "amount" | colnames(recipe)[2] != "unit" | colnames(recipe)[3] != "ingredient"){
+    stop("Column names are invalid. They should be 'amount', 'unit', and 'ingredient'.")
+  }
+  nrow_recipe <- nrow(recipe)
+  ncol_recipe <- ncol(recipe)
+  for (i in 1:nrow_recipe){
+    if (recipe[i,2] %in% c("cup", "cups")){
+      recipe[i,1] <- recipe[i,1] * 236.6
+      recipe[i,2] <- "ml"
+    } else {
+      if (recipe[i,2] == "oz"){
+        recipe[i,1] <- recipe[i,1] * 28.3
+        recipe[i,2] <- "gr"
+        }
+      }
+    
+  }
+  
+  
+  
+  return recipe.metric
 }
+
+sample_1 <- data.frame(amount = c(3, 4), unit = c("cups", "oz"), ingredient = c("flour", "milk"))
+recipeConversion(sample_1)
+sample_2 <- data.frame(total = c(3, 4), unit = c("cups", "oz"), ingredients = c("flour", "milk"))
+recipeConversion(sample_2)
 
 
 #### Function #4a
@@ -89,9 +144,23 @@ recipeConversion <- function(recipe){
 # -- Calculate, and store, the mean of this bootstrap sample, call that mu_i (i in 1:B)
 # -- The bootstrap variance is the sample variance of mu_1, mu_2, ..., mu_B
 
-bootstrapVarEst <- function(x, B){
 
+bootstrapVarEst <- function(x, B){
+  mu_i <- c()
+  for (i in 1:B){
+    bootstrap_sample <- sample(x, length(x), replace = TRUE)
+    boot_mean <- mean(bootstrap_sample)
+    mu_i <- append(mu_i, boot_mean)
+  }  
+  boot.sigma2.est <- var(mu_i)
+  return (boot.sigma2.est)
 }
+
+
+#use for loop but not using i
+#when that is the case, you use the replicate function
+#replicate(n, function), where this replicates a function n times
+
 
 #### Function #4b
 #### Implement the function "jackknifeVarEst"
@@ -111,9 +180,21 @@ bootstrapVarEst <- function(x, B){
 #     for this reduced sample calculate the sample mean (get mu_1, mu_2, ..., mu_n)
 # -- The jackknife variance is the sample variance of mu_1, mu_2, ..., mu_n
 
-jackknifeVarEst <- fuction(x){
-
+jackknifeVarEst <- function(x){
+  mu_i <- c()
+  n <- length(x)
+  for (i in 1:n){
+    jackknife_sample <- sample(x, length(x), replace = TRUE)
+    jackknife_sample <- jackknife_sample[-i]
+    jack_mean <- mean(jackknife_sample)
+    mu_i <- append(mu_i, jack_mean)
+  }  
+  jack.sigma2.est <- var(mu_i)
+  return (jack.sigma2.est)
 }
+
+# use a for loop and in loop you remove ith value x[-i]
+
 
 #### Function #4c
 #### Implement the function "samplingVarEst"
